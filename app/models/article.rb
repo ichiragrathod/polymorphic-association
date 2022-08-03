7a199dab6abd2bcccea835e5555001acd13d02d9
chirag <chirag.r@simformsolutions.com>
2022-08-03 17:03:31 +0530

    Run rubocop for proper ruby code structure.

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,13 +8,12 @@ class Article < ApplicationRecord
 
   # Validation for image files
   validates :image, :pdf, presence: true
-  validates :image, file_size: { less_than_or_equal_to: 1.megabytes, message: "should be less than 1MB." },
-  file_content_type: { allow: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'],
-  message: "type must be in JPG, JPEG, PNG format."}
+  validates :image, file_size: { less_than_or_equal_to: 1.megabytes, message: 'should be less than 1MB.' },
+                    file_content_type: { allow: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'],
+                                         message: 'type must be in JPG, JPEG, PNG format.' }
 
   # Validation for pdf files
-  validates :pdf, file_size: { less_than_or_equal_to: 5.megabytes, message: "should be less than 5MB." },
-  file_content_type: { allow: ['application/pdf'],
-  message: "File format invalid"}
-  
+  validates :pdf, file_size: { less_than_or_equal_to: 5.megabytes, message: 'should be less than 5MB.' },
+                  file_content_type: { allow: ['application/pdf'],
+                                       message: 'File format invalid' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments, module: :articles
   end
-  
+
   resources :events do
     resources :comments, module: :events
   end
