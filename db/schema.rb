0bda18fbc678771b8cc0a65fdc942542c2ce71d5
chirag <chirag.r@simformsolutions.com>
2022-08-05 19:32:21 +0530

    Removed ENV Variable Errors.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_02_061508) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -48,7 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_02_061508) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commentable_type", null: false
-    t.integer "commentable_id", null: false
+    t.bigint "commentable_id", null: false
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
