96e09a467477caa112d43485a217567b65fa7c8f
chirag <chirag.r@simformsolutions.com>
2022-08-08 17:31:36 +0530

    Implemented Bootstrap and validation in form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, :last_name, :religion, :street_address, :city, :country, :postal_code, :phone_number, :mobile_number, :gender, presence: true       
+  validates :mobile_number, :phone_number, numericality: { only_integer: true }, length:{is:10}
+  validates :postal_code, numericality: { only_integer: true }, length:{is:6}
 end
