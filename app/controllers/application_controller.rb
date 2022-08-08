8bd74e49950481a29d2b5097457029e0f87826a1
chirag <chirag.r@simformsolutions.com>
2022-08-08 16:44:28 +0530

    added custom attributes in user model.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  
+  def configure_permitted_parameters
+    attributes = [:first_name, :last_name, :religion, :street_address, :city, :country, :postal_code, :phone_number, :mobile_number, :gender]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_08_075014) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_08_092837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,6 +73,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_08_075014) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "religion"
+    t.string "street_address"
+    t.string "city"
+    t.string "country"
+    t.string "postal_code"
+    t.string "phone_number"
+    t.string "mobile_number"
+    t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
